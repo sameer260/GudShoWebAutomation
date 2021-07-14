5b8c0c13a15096f684283f573424eb3e429d14e5
user <user@DESKTOP-BG85F12>
2021-07-14 19:01:32 +0530

    tags removed from runner class

--- WebAutomation/src/test/java/Runner/RunnerTest.java
+++ WebAutomation/src/test/java/Runner/RunnerTest.java
@@ -25,8 +25,7 @@ import net.masterthought.cucumber.sorting.SortingMethod;
 	
 	@RunWith(Cucumber.class)
 	@CucumberOptions(features= "src/test/resources/Features/frontend/SmokeTest.feature",
-	glue={"steps"},
-	tags="@paywall",		
+	glue={"steps"},	
 	plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 			  "pretty","json:target/cucumber-reports/Cucumber.json", 
 			  "junit:target/cucumber-reports/Cucumber.xml",
--- WebAutomation/src/test/java/steps/frontend/SmokeTest.java
+++ WebAutomation/src/test/java/steps/frontend/SmokeTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import Pageobjects.frontend.ShareFeature;
 import Pageobjects.frontend.ToastandErrormessages;
 import Pageobjects.frontend.commonlocatorsandmethods;
